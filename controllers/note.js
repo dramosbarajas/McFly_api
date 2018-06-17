8b6de6491dd2c6c599d5b8cac2b584fdb3fd7a93
David Ramos <dramosbarajas@gmail.com>
2018-06-17 20:05:26 +0200

    Creación de las funciones que manejaran las llamadas del fichero de rutas

--- controllers/note.js
+++ controllers/note.js
@@ -2,9 +2,34 @@
 
 const Note = require('../models/note')
 
+//--GET--Recupera todas las notas que haya en la BD
 function getAllNotes(req,res) {
+
+}
+//--GET-- Recupera todas las notas que sean favoritas
+function getAllFavNotes(req,res) {
     res.send("API funcionando getAllNotes")
 }
+//--GET-- Recupera una unica nota por un determinado ID 
+
+function getNoteById(req,res) {
+    res.send("API funcionando getAllNotes")
+}
+//--POST-- Crea una nota nueva
+function createNote(req,res) {
+    res.send("API funcionando getAllNotes")
+}
+//--DELETE-- Borra una nota por un ID
+
+function deleteNote(req,res) {
+    res.send("API funcionando getAllNotes")
+}
+//--PATCH-- Update de una nota a favorita o la desmarca como fav
+
+function updateNote(req,res) {
+    res.send("API funcionando getAllNotes")
+}
+
 
 module.exports = {
     getAllNotes,
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const NoteController = require ('../controllers/note')
 const api = express.Router()
 
+//Rutas para los endpoint
 api.get('/note', NoteController.getAllNotes)
+api.get('/note/:noteId', NoteController.getNoteById)
+api.get('/note', NoteController.getAllFavNotes)
+api.post('/note', NoteController.createNote)
+api.delete('/note/:noteId', NoteController.deleteNote)
+api.patch('/note/:noteId', NoteController.updateNote)
 
 module.exports = api
