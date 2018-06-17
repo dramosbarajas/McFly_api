af917a1826424abfd98a4a3815ae62a8e28ba31e
David Ramos <dramosbarajas@gmail.com>
2018-06-17 19:31:17 +0200

    Añadido modelo Note

---
+++ models/notes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const moongose = require('mongoose')
+const Schema = moongose.Schema
+
+const NotesSchema = Schema ({
+    title = String,
+    content = String, 
+    favorite = Boolean,
+})
+
+module.exports = moongose.model('Note' , NotesSchema)
